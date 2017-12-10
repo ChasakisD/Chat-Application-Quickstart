e204adaba02e04159c3d2d2e6769e72676ca6fce
ChasakisD <chasakisd@gmail.com>
2017-12-10 22:56:55 +0200

    Added chathub and mvc application

---
+++ src/ChatApplication.Server/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatApplication.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string username, string message, string groupname)
+        {
+            Clients.Group(groupname).GetMessage(username, message);
+        }
+
+        public Task JoinGroup(string name)
+        {
+            return Groups.Add(Context.ConnectionId, name);
+        }
+
+        public Task LeaveGroup(string name)
+        {
+            return Groups.Remove(Context.ConnectionId, name);
+        }
+    }
+}
---
+++ src/ChatApplication.Server/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatApplication.Server.Startup))]
+namespace ChatApplication.Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
